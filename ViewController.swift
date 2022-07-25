76310c315b03e7701016559fbf41506d37495b4a
dbguswls030 <dbguswls030@gmail.com>
2022-07-25 10:41:11 +0900

    feat: 탭바 초기 설정(홈, 동네생활, 내 근처, 채팅, 나의 당근)

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDaangnMarket
+//
+//  Created by 유현진 on 2022/07/25.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
